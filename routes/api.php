613178baf64e93bf4955d1e80d81be18b33c536c
JuanCM <juancampuzano2356@gmail.com>
2024-10-18 11:26:42 -0500

    Se finaliza la clase 12

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,14 @@
 <?php
 
-use App\Http\Controllers\Api\V1\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\V1\PostController as PostV1;
+use App\Http\Controllers\Api\V2\PostController as PostV2;
 
 Route::prefix('v1')->group(function(){
-    Route::apiResource('posts', PostController::class);
+    Route::apiResource('posts', PostV1::class)->only(['index', 'show', 'destroy']);
+});
+
+Route::prefix('v2')->group(function(){
+    Route::apiResource('posts', PostV2::class)->only(['index', 'show', 'destroy']);
 });
