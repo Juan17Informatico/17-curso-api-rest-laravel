4cd4bc90f84244ea8b5224d7ad0b5378b2390970
JuanCM <juancampuzano2356@gmail.com>
2024-11-08 13:53:13 -0500

    Se finaliza el curso

--- app/Http/Controllers/Api/LoginController.php
+++ app/Http/Controllers/Api/LoginController.php
@@ -12,17 +12,25 @@ class LoginController extends Controller
     public function login(Request $request) {
         $this->validateLogin($request);
 
-        // Login True
 
-        // Login False
+        if( Auth::attempt($request->only('email', 'password'))) {
+            return response()->json([
+                'token' => $request->user()->createToken($request->name)->plainTextToken,
+                'message' => 'Success'
+            ]);
+        }
+
+        return response()->json([
+            'message' => 'Unauthorized',
+        ], 401);
 
     }
 
-    public function register(Request $request) {
+    public function validateLogin(Request $request) {
         return $request->validate([
-            'email' => 'required|email',
+            'email' =>    'required|email',
             'password' => 'required',
-            'name' => 'required',
+            'name' =>     'required',
         ]);
     }
 
